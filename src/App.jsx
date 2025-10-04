d0752ee6e1dd17069c81bde376b9ce741b9a909a
rvcjourney <ramvish958@gmail.com>
2025-10-04 12:10:47 +0530

    go

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as  Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import About from './pages/About.jsx';
 import Home from './pages/Home.jsx';
 import TotalBrands from './pages/TotalBrands.jsx';
 import Company from './pages/Company.jsx';
 import NewsGallery from './pages/NewsGallery.jsx';
-import Contact from './pages/Contact.jsx'; 
+import Contact from './pages/Contact.jsx';
 import Career from './components/Career.jsx';
 import Footer from './components/Footer.jsx';
 import ScrollToTop from './components/ScrollToTop.jsx';
@@ -13,15 +13,15 @@ import ScrollToTop from './components/ScrollToTop.jsx';
 function App() {
   return (
     <BrowserRouter basename="/maini">
-      <ScrollToTop/>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/brands" element={<TotalBrands />} />
         <Route path="/company" element={<Company />} />
-        <Route path="/newsgallery" element={< NewsGallery/>} />
-        <Route path="/contact" element={<Contact />} /> 
+        <Route path="/newsgallery" element={< NewsGallery />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/career" element={<Career />} />
       </Routes>
       <Footer />
